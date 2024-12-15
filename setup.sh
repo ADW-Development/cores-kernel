8b2a4be65ea8923e39a287494f75f6aa2bd75e45
T_cat9000_2 <terpstragraham@gmail.com>
2024-12-15 11:37:17 -0800

    paths

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ echo "welcome to the setup! lets get your credentials."
 read -p "enter your username:" USERNAME
 read -sp "enter your password: " PASSWORD
 
-CRED_FILE="C:/Users/$(whoami)/downloads/coreskernel-0.0.1/cred_file.txt"
+CRED_FILE="C:/Users/$(whoami)/downloads/cores-kernel-0.0.1/cred_file.txt"
 
 echo "$USERNAME" > "$CRED_FILE"
 echo "$PASSWORD" >> "$CRED_FILE"
